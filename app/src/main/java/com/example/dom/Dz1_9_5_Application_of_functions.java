8b2f6ec93bf3b3fbe26bd5085ad7cc67cc694f46
Simbaleon <56549732+Simbaleon@users.noreply.github.com>
2019-11-01 14:15:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dom/Dz1_9_5_Application_of_functions.java
@@ -0,0 +1,45 @@
+package com.example.dom;
+
+import android.content.Context;
+import android.graphics.*;
+import android.view.View;
+
+public class Dz1_9_5_Application_of_functions extends View {
+    int N = 10;
+    int j=0;
+    Paint paint = new Paint();
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        addValues(x, vx);
+        addValues(y, vy);
+
+        invalidate();
+    }
+    float rand(float min , float max){
+        return (float)(Math.random() * (max - min + 1)) + min;
+    }
+    void fillRandom(float[] array , float min, float max){
+        for (int i = 0; i < array.length; i++){
+            array[i] = rand (min, max);
+        }
+    }
+    void addValues(float[] array , float[] values){
+        for (int i = 0; i < array.length; i++){
+            array[i] += values[i];
+        }
+    }
+    public Dz1_9_5_Application_of_functions(Context context) {
+        super(context);
+        fillRandom(x, 0, 500);
+        fillRandom(y, 0, 500);
+        fillRandom(vx, -3, 3);
+        fillRandom(vy, -3, 3);
+    }
+}
