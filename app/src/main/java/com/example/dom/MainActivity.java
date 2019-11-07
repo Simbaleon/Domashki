0d19149ffd40e86c9154cee14af586e62bb75099
Polina Belikova <simbaleon1980@gmail.com>
2019-11-07 22:01:29 +0300

    Dz1_7_6_1

--- app/src/main/java/com/example/dom/MainActivity.java
+++ app/src/main/java/com/example/dom/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new  Dz1_8_11_Bolls (this));
+        setContentView(new Dz1_7_6_1 (this));
     }
 }
