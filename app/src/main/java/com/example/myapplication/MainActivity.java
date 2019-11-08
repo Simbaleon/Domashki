1a38bf9a1fdc7a48128f7818b415f214355c6cf4
Polina Belikova <simbaleon1980@gmail.com>
2019-11-08 12:24:48 +0300

    Dz1_9_5_Application_of_functions

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<< HEAD
-import com.example.dom.Dz1_7_7;
-=======
->>>>>>> develop
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,22 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        setContentView(new Dz1_7_7(this));
-=======
-        setContentView(new Dz1_8_7_Simple_animation(this));
->>>>>>> develop
-=======
-        setContentView(new Dz1_8_8_Function_graphs(this));
->>>>>>> develop
-=======
-        setContentView(new Dz1_8_9_Model_Solar_System(this));
->>>>>>> develop
-=======
-        setContentView(new Dz1_8_10_Pendulums(this));
->>>>>>> develop
+        setContentView(new Dz1_9_5_Application_of_functions(this));
     }
 }
