2511b57d0d759daccbe3c0f04febcf7be39a1109
Polina Belikova <simbaleon1980@gmail.com>
2019-11-08 12:09:54 +0300

    Dz1_8_8_Function_graphs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new Dz1_8_7_Simple_animation(this));
+        setContentView(new Dz1_8_8_Function_graphs(this));
     }
 }
