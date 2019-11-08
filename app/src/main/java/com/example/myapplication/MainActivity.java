8987e5377b5eec5cde9a3e84ed3f58234732bce3
Polina Belikova <simbaleon1980@gmail.com>
2019-11-08 12:14:07 +0300

    Dz1_8_9_Model_Solar_System

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new Dz1_8_8_Function_graphs(this));
+        setContentView(new Dz1_8_9_Model_Solar_System(this));
     }
 }
