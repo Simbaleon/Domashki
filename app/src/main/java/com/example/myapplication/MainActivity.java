7eaad618d2981bd1e98ece8f1aa141be13980cf9
Polina Belikova <simbaleon1980@gmail.com>
2019-11-08 12:18:50 +0300

    Dz1_8_11_Bolls

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new Dz1_8_10_Pendulums(this));
+        setContentView(new Dz1_8_11_Bolls(this));
     }
 }
